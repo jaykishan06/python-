7d4108c8fb9d2c9b221dd0f7853b0c384872269b
JAYKISHAN VISHWAKARMA <jaykishanvishwakarma06@gmail.com>
2026-09-17 16:38:21 +0530

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,1 +1,16 @@
-created stack.py 
+stack = []
+
+# Push
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print("Stack:", stack)
+
+# Peek
+print("Top element:", stack[-1])
+
+# Pop
+print("Popped:", stack.pop())
+
+print("Stack after pop:", stack)
