9de2c56cc30c772a1d83defcc30fd63e3c56f732
JAYKISHAN VISHWAKARMA <jaykishanvishwakarma06@gmail.com>
2026-09-18 17:55:31 +0530

    Add number guessing game functionality

--- game.py
+++ game.py
@@ -1,1 +1,12 @@
- 
+ # Number Guessing Game
+
+secret_number = 7
+
+guess = int(input("Guess the number (1-10): "))
+
+if guess == secret_number:
+    print("Correct! 🎉")
+elif guess < secret_number:
+    print("Too low!")
+else:
+    print("Too high!")
