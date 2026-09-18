f17c2c9da329b0ee5185f41baa180a20e2dd5406
JAYKISHAN VISHWAKARMA <jaykishanvishwakarma06@gmail.com>
2026-09-18 17:54:51 +0530

    Implement match case for day of the week

--- switchcase.py
+++ switchcase.py
@@ -1,1 +1,21 @@
-  
+  # Match Case in Python
+
+day = int(input("Enter day number (1-7): "))
+
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:
+        print("Invalid day")
