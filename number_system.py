b50b664d96fa51a0344a558790cdda1314c40ef0
JAYKISHAN VISHWAKARMA <jaykishanvishwakarma06@gmail.com>
2026-09-18 17:56:13 +0530

    Add number system conversion for decimal input

--- number_system.py
+++ number_system.py
@@ -1,1 +1,7 @@
-  
+  # Number System Conversion
+
+number = int(input("Enter a decimal number: "))
+
+print("Binary:", bin(number))
+print("Octal:", oct(number))
+print("Hexadecimal:", hex(number))
